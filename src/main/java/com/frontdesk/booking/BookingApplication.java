2e3de211fe1783b5997ae7b8859eeaf98050b4ba
Abinaya R <abiraghuraman@gmail.com>
2020-05-24 13:12:22 +0530

    Test

--- src/main/java/com/frontdesk/booking/BookingApplication.java
+++ src/main/java/com/frontdesk/booking/BookingApplication.java
@@ -2,7 +2,7 @@ package com.frontdesk.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*this is the main application */
 @SpringBootApplication
 public class BookingApplication {
 
